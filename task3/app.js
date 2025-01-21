6591f8fa7c0436c1577135a8ff422b83313db6b0
mutha pavani <muttapavani9gmail.com>
2025-01-21 11:39:03 +0530

    push

--- task3/app.js
+++ task3/app.js
@@ -1,12 +1,11 @@
 express=require("express")
-var connection=require("./database.js")
 var app=express();
-var router=require("/user.js")
+var router=require("/data.js")
 app.use("/auth",router)
 
 app.get("/",(req,res)=>{
     res.send("hi this is root api")
 })
-app.listen(3302,()=>{
+app.listen(3303,()=>{
     console.log("server running")
 })
